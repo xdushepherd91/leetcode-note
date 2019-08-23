ec348620ed25473d173674d985a0d61b033a7107
wangqy <wangqy@geovis.com.cn>
2019-08-23 08:47:46 +0800

    生成README及生成博客模板脚本

---
+++ note/leetcode-238.md
@@ -0,0 +1,10 @@
+### 题目
+
+[[leetcode 238]](https://leetcode-cn.com/problems/delete-node-in-a-linked-list/submissions/) 删除链表中的节点
+
+### 描述
+
+### 解析
+
+### 实现
+
---
+++ pg.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+
+cat > ./note/leetcode-$1.md << EOF
+### 题目
+
+[[leetcode $1]]($3) $2
+
+### 描述
+
+### 解析
+
+### 实现
+
+EOF
+
+echo -e "\n[leetcode-$1](./note/leetcode-$1.md) $2" >> README.md
+
